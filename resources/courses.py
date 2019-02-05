5a463afe86c966d7dbf439eecc46d2e2aed2a50d
PaCo <conte.pas@me.com>
2019-02-05 17:34:39 +0100

    Add resource courses

---
+++ resources/courses.py
@@ -0,0 +1,44 @@
+from flask import jsonify, Blueprint
+from flask_restful import Resource, Api, reqparse
+import models
+
+class CourseList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'title',
+            required=True,
+            help='No course title provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'url',
+            required=True,
+            help='No course url provided',
+            location=['form','json']
+        )
+        super().__init__()
+
+    def get(self):
+        return jsonify({'courses': [{'title':'Python'}]})
+
+class Course(Resource):
+    def get(self, id):
+        return jsonify({'title': 'Python'})
+    def put(self, id):
+        return jsonify({'title': 'Python'})
+    def delete(self, id):
+        return jsonify({'title': 'Python'})
+
+courses_api = Blueprint('resources.courses', __name__)
+api = Api(courses_api)
+api.add_resource(
+    CourseList,
+    '/courses',
+    endpoint='courses'
+)
+api.add_resource(
+    Course,
+    '/courses<int:id>',
+    endpoint='course'
+)
